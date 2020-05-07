85747ee3a08199ebc1ddfca4b2d0f1cb8ff5cf3b
sanjay-btc <sanjay.prajapati@botreetechnologies.com>
2020-05-07 14:36:34 +0530

    Home page with multiple file uploads.

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,26 @@
+require 'fileutils'
+
+class GamesController < ApplicationController
+  def index
+    cookies[:image_urls] = nil
+  end
+
+  def store_images
+    cookies[:image_urls] = img_public_paths if params[:image].present?
+  end
+
+  private
+
+  def copy_file_to_public(image)
+    tmp = image.tempfile
+    file = File.join('public', image.original_filename)
+    FileUtils.cp tmp.path, file
+  end
+
+  def img_public_paths
+    params[:image].each_with_object([]) do |image, image_urls|
+      copy_file_to_public(image)
+      image_urls << image.original_filename
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  match 'games/store_images' => 'games#store_images', via: %i[get post]
+  root to: 'games#index'
 end
