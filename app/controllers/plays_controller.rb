b59d27abf6d1a948060ad26846d6d60d726a8a8d
sanjay-btc <sanjay.prajapati@botreetechnologies.com>
2020-05-07 19:50:47 +0530

    Append row to table when new record is created

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -1,7 +1,12 @@
 class PlaysController < ApplicationController
   skip_before_action :verify_authenticity_token, only:[:create]
   def create
-    Play.create(play_params)
+    @play = Play.new(play_params)
+    if @play.save
+      respond_to do |format|
+        format.json { render json: {url: @play.image_url, count: @play.counter} }
+      end
+    end
   end
 
   private
